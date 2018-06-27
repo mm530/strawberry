cf748fed6f169bbb1dc9d2b775ae6cce49344974
mm530 <mm5303344@163.com>
2018-06-28 02:10:15 +0800

    只在乎把代码写的好看，容易读。不在乎官方语法怎么变。

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+PACKAGES = [
+    'strawberry',
+]
+
+REQUIRES = [
+]
+
+setup(
+    name='strawberry',
+    version='1.0.1',
+    description='',
+    long_description='',
+    author='mm530',
+    author_email='mm5303344@163.com',
+    url='https://github.com/mm530/strawberry',
+    include_package_data=True,
+    install_requires=REQUIRES,
+    zip_safe=False,
+    packages=PACKAGES,
+)
